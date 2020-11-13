69a26465c8177beb14ca14b5eae184d1cc83ca60
Constantine <barr.develop@gmail.com>
2020-11-13 23:39:53 +0400

    add favorites and cart pages

---
+++ src/pages/favorites/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+import Layout from "components/Layout";
+
+function Favorites() {
+  return (
+    <Layout>
+      <Head>
+        <title>Favorites</title>
+      </Head>
+
+      <h1>Favorites</h1>
+      <div></div>
+    </Layout>
+  );
+}
+
+export default Favorites;
