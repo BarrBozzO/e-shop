5f50674b65cd23e078b90e2ce7d54055c86e7d49
Constantine <barr.develop@gmail.com>
2020-11-22 00:33:21 +0400

    add links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,7 @@ function Footer() {
       css={{
         padding: "72px 0 54px",
         backgroundColor: "rgb(228, 228, 228)",
+        marginTop: "2rem",
       }}
     >
       <div
@@ -25,14 +26,8 @@ function Footer() {
             alignItems: "flex-start",
           }}
         >
-          <div>
-            <h4
-              css={{
-                textTransform: "uppercase",
-              }}
-            >
-              SHOP
-            </h4>
+          <div css={linksColumn}>
+            <h4>shop</h4>
             <Link href="/products/women">
               <a css={linkCSS}>Women</a>
             </Link>
@@ -43,21 +38,23 @@ function Footer() {
               <a css={linkCSS}>Kids</a>
             </Link>
           </div>
-          <div
-            css={{
-              marginLeft: "2rem",
-            }}
-          >
-            <h4
-              css={{
-                textTransform: "uppercase",
-              }}
-            >
-              Become a member
-            </h4>
+          <div css={linksColumn}>
+            <h4>help</h4>
+            <Link href="/terun">
+              <a css={linkCSS}>Return and refund policy</a>
+            </Link>
+            <Link href="/terms">
+              <a css={linkCSS}>Terms and conditions</a>
+            </Link>
+            <Link href="/privacy">
+              <a css={linkCSS}>Privacy Notice</a>
+            </Link>
+          </div>
+          <div css={linksColumn}>
+            <h4>Become a member</h4>
             <p>Join now and get 10% off your next purchase!</p>
             <Link href="/member">
-              <a css={linkCSS}>Read more</a>
+              <a css={[linkCSS, readMoreCSS]}>Read more →</a>
             </Link>
           </div>
         </div>
@@ -70,6 +67,15 @@ function Footer() {
       >
         <Icon name="full-logo" css={{ width: "100%", height: "24px" }} />
       </div>
+      <div
+        css={{
+          textTransform: "uppercase",
+          textAlign: "center",
+          color: "#9a9a9a",
+        }}
+      >
+        DEMO project for presentation only purposes
+      </div>
     </footer>
   );
 }
@@ -77,6 +83,25 @@ function Footer() {
 const linkCSS = css`
   display: block;
   margin-bottom: 1rem;
+`;
+
+const readMoreCSS = css`
+  text-transform: uppercase;
+  font-weight: 700;
+
+  &:hover {
+    color: #e50010;
+  }
+`;
+
+const linksColumn = css`
+  h4 {
+    text-transform: uppercase;
+  }
+
+  & + & {
+    margin-left: 2rem;
+  }
 `;
 
 export default Footer;
