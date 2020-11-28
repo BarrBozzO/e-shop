93f26729504f728b589c30280432131ba43aae2c
Constantine <barr.develop@gmail.com>
2020-11-28 23:47:52 +0400

    add seo pages: 1. membership 2. privacy 3. returns 4. terms

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,107 +1,110 @@
-import React from "react";
-import Link from "next/link";
-import { css } from "@emotion/core";
-import Icon from "components/Icon";
+import React from 'react';
+import Link from 'next/link';
+import { css } from '@emotion/core';
+import Icon from 'components/Icon';
 
 function Footer() {
-  return (
-    <footer
-      css={{
-        padding: "72px 0 54px",
-        backgroundColor: "rgb(228, 228, 228)",
-        marginTop: "2rem",
-      }}
-    >
-      <div
-        css={{
-          maxWidth: "960px",
-          margin: "0 auto",
-        }}
-      >
-        <div
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "flex-start",
-          }}
+    return (
+        <footer
+            css={{
+                padding: '72px 0 54px',
+                backgroundColor: 'rgb(228, 228, 228)',
+                marginTop: '2rem'
+            }}
         >
-          <div css={linksColumn}>
-            <h4>shop</h4>
-            <Link href="/products/women">
-              <a css={linkCSS}>Women</a>
-            </Link>
-            <Link href="/products/men">
-              <a css={linkCSS}>Men</a>
-            </Link>
-            <Link href="/products/kids">
-              <a css={linkCSS}>Kids</a>
-            </Link>
-          </div>
-          <div css={linksColumn}>
-            <h4>help</h4>
-            <Link href="/terun">
-              <a css={linkCSS}>Return and refund policy</a>
-            </Link>
-            <Link href="/terms">
-              <a css={linkCSS}>Terms and conditions</a>
-            </Link>
-            <Link href="/privacy">
-              <a css={linkCSS}>Privacy Notice</a>
-            </Link>
-          </div>
-          <div css={linksColumn}>
-            <h4>Become a member</h4>
-            <p>Join now and get 10% off your next purchase!</p>
-            <Link href="/member">
-              <a css={[linkCSS, readMoreCSS]}>Read more →</a>
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div
-        css={{
-          width: "200px",
-          margin: "2rem auto",
-        }}
-      >
-        <Icon name="full-logo" css={{ width: "100%", height: "24px" }} />
-      </div>
-      <div
-        css={{
-          textTransform: "uppercase",
-          textAlign: "center",
-          color: "#9a9a9a",
-        }}
-      >
-        DEMO project for presentation only purposes
-      </div>
-    </footer>
-  );
+            <div
+                css={{
+                    maxWidth: '960px',
+                    margin: '0 auto'
+                }}
+            >
+                <div
+                    css={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'flex-start'
+                    }}
+                >
+                    <div css={linksColumn}>
+                        <h4>shop</h4>
+                        <Link href="/products/women">
+                            <a css={linkCSS}>Women</a>
+                        </Link>
+                        <Link href="/products/men">
+                            <a css={linkCSS}>Men</a>
+                        </Link>
+                        <Link href="/products/kids">
+                            <a css={linkCSS}>Kids</a>
+                        </Link>
+                    </div>
+                    <div css={linksColumn}>
+                        <h4>help</h4>
+                        <Link href="/returns-and-refund">
+                            <a css={linkCSS}>Return and refund policy</a>
+                        </Link>
+                        <Link href="/terms">
+                            <a css={linkCSS}>Terms and conditions</a>
+                        </Link>
+                        <Link href="/privacy">
+                            <a css={linkCSS}>Privacy Notice</a>
+                        </Link>
+                    </div>
+                    <div css={linksColumn}>
+                        <h4>Become a member</h4>
+                        <p>Join now and get 10% off your next purchase!</p>
+                        <Link href="/member">
+                            <a css={[linkCSS, readMoreCSS]}>Read more →</a>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div
+                css={{
+                    width: '200px',
+                    margin: '2rem auto'
+                }}
+            >
+                <Icon
+                    name="full-logo"
+                    css={{ width: '100%', height: '24px' }}
+                />
+            </div>
+            <div
+                css={{
+                    textTransform: 'uppercase',
+                    textAlign: 'center',
+                    color: '#9a9a9a'
+                }}
+            >
+                DEMO project for presentation only purposes
+            </div>
+        </footer>
+    );
 }
 
 const linkCSS = css`
-  display: block;
-  margin-bottom: 1rem;
+    display: block;
+    margin-bottom: 1rem;
 `;
 
 const readMoreCSS = css`
-  text-transform: uppercase;
-  font-weight: 700;
+    text-transform: uppercase;
+    font-weight: 700;
 
-  &:hover {
-    color: #e50010;
-  }
+    &:hover {
+        color: #e50010;
+    }
 `;
 
 const linksColumn = css`
-  h4 {
-    text-transform: uppercase;
-  }
+    h4 {
+        text-transform: uppercase;
+    }
 
-  & + & {
-    margin-left: 2rem;
-  }
+    & + & {
+        margin-left: 2rem;
+    }
 `;
 
 export default Footer;
--- src/features/cart/Order.jsx
+++ src/features/cart/Order.jsx
@@ -1,177 +1,187 @@
-import React from "react";
-import Link from "next/link";
-import { css } from "@emotion/core";
-import Button from "components/Button";
+import React from 'react';
+import Link from 'next/link';
+import { css } from '@emotion/core';
+import Button from 'components/Button';
 
 const SHIPPING_PRICE = 100;
 
 function Order({ products }) {
-  const productsPrice = products.reduce((total, currentProduct) => {
-    let value =
-      Number.parseFloat(currentProduct?.data?.price?.value) *
-      currentProduct.__count;
-    if (isNaN(value)) {
-      throw new Error("Appliaction failed");
-    }
-    return total + value;
-  }, 0);
+    const productsPrice = products.reduce((total, currentProduct) => {
+        let value =
+            Number.parseFloat(currentProduct?.data?.price?.value) *
+            currentProduct.__count;
+        if (isNaN(value)) {
+            throw new Error('Appliaction failed');
+        }
+        return total + value;
+    }, 0);
 
-  const getTotal = () => {
-    return (SHIPPING_PRICE + productsPrice).toFixed(2);
-  };
+    const getTotal = () => {
+        return (SHIPPING_PRICE + productsPrice).toFixed(2);
+    };
 
-  return (
-    <section
-      css={{
-        flex: "0 0 auto",
-        maxWidth: "360px",
-      }}
-    >
-      <div
-        css={{
-          backgroundColor: "#fff",
-          padding: "1rem",
-        }}
-      >
-        <h2 css={titleCSS}>Make order</h2>
-        <div>
-          <div css={orderCSS}>
+    return (
+        <section
+            css={{
+                flex: '0 0 auto',
+                maxWidth: '360px'
+            }}
+        >
             <div
-              css={{
-                flex: "1 0 50%",
-              }}
+                css={{
+                    backgroundColor: '#fff',
+                    padding: '1rem'
+                }}
             >
-              Order
+                <h2 css={titleCSS}>Make order</h2>
+                <div>
+                    <div css={orderCSS}>
+                        <div
+                            css={{
+                                flex: '1 0 50%'
+                            }}
+                        >
+                            Order
+                        </div>
+                        <div
+                            css={{
+                                flex: '1 0 50%',
+                                textAlign: 'right'
+                            }}
+                        >
+                            ${productsPrice.toFixed(2)}
+                        </div>
+                    </div>
+                    <div css={shippingCSS}>
+                        <div
+                            css={{
+                                flex: '1 0 50%'
+                            }}
+                        >
+                            Shipping Value
+                        </div>
+                        <div
+                            css={{
+                                flex: '1 0 50%',
+                                textAlign: 'right'
+                            }}
+                        >
+                            ${SHIPPING_PRICE}
+                        </div>
+                    </div>
+                    <div css={totalCSS}>
+                        <div
+                            css={{
+                                flex: '1 0 50%'
+                            }}
+                        >
+                            Total
+                        </div>
+                        <div
+                            css={{
+                                flex: '1 0 50%',
+                                textAlign: 'right'
+                            }}
+                        >
+                            ${getTotal()}
+                        </div>
+                    </div>
+                </div>
+                <Button>Continue to checkout</Button>
+                <p css={discountCSS}>
+                    Become a member to get early access to Black Friday + a 10%
+                    off welcome offer!
+                </p>
+                <p css={taxCSS}>
+                    The estimated tax will be confirmed once you added your
+                    shipping address in checkout. 30-day returns. Read more
+                    about our{' '}
+                    <Link href="/returns-and-refund">
+                        <a
+                            css={{
+                                textDecoration: 'underline'
+                            }}
+                        >
+                            return and refund policy
+                        </a>
+                    </Link>
+                    .
+                </p>
             </div>
-            <div
-              css={{
-                flex: "1 0 50%",
-                textAlign: "right",
-              }}
-            >
-              ${productsPrice.toFixed(2)}
+            <div css={shippingDescriptionCSS}>
+                Shipping Update: All orders will take 12-15 business days to be
+                delivered. Please take this into consideration before placing
+                your order. We appreciate your patience and understanding during
+                this time.
+                <h3>Shipping & Returns</h3>
+                Please ensure you have entered the correct shipping address, as
+                this cannot be changed after you have placed an order. Please
+                note, an order cannot be cancelled or modified once it has been
+                placed.
+                <h3>SHIPPING OPTIONS:</h3>
+                FREE STANDARD SHIPPING OVER $40 $3.99 below $40 (12-15 business
+                days)
+                <h3>RETURNS</h3>
+                Free in-store returns Return with USPS ($5.99) H&M HOME items
+                purchased online must be returned by mail
             </div>
-          </div>
-          <div css={shippingCSS}>
-            <div
-              css={{
-                flex: "1 0 50%",
-              }}
-            >
-              Shipping Value
-            </div>
-            <div
-              css={{
-                flex: "1 0 50%",
-                textAlign: "right",
-              }}
-            >
-              ${SHIPPING_PRICE}
-            </div>
-          </div>
-          <div css={totalCSS}>
-            <div
-              css={{
-                flex: "1 0 50%",
-              }}
-            >
-              Total
-            </div>
-            <div
-              css={{
-                flex: "1 0 50%",
-                textAlign: "right",
-              }}
-            >
-              ${getTotal()}
-            </div>
-          </div>
-        </div>
-        <Button>Continue to checkout</Button>
-        <p css={discountCSS}>
-          Become a member to get early access to Black Friday + a 10% off
-          welcome offer!
-        </p>
-        <p css={taxCSS}>
-          The estimated tax will be confirmed once you added your shipping
-          address in checkout. 30-day returns. Read more about our{" "}
-          <Link href="/return">
-            <a>return and refund policy</a>
-          </Link>
-          .
-        </p>
-      </div>
-      <div css={shippingDescriptionCSS}>
-        Shipping Update: All orders will take 12-15 business days to be
-        delivered. Please take this into consideration before placing your
-        order. We appreciate your patience and understanding during this time.
-        <h3>Shipping & Returns</h3>
-        Please ensure you have entered the correct shipping address, as this
-        cannot be changed after you have placed an order. Please note, an order
-        cannot be cancelled or modified once it has been placed.
-        <h3>SHIPPING OPTIONS:</h3>
-        FREE STANDARD SHIPPING OVER $40 $3.99 below $40 (12-15 business days)
-        <h3>RETURNS</h3>
-        Free in-store returns Return with USPS ($5.99) H&M HOME items purchased
-        online must be returned by mail
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
 
 const titleCSS = css`
-  text-transform: uppercase;
+    text-transform: uppercase;
 `;
 
 const shippingCSS = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 1rem;
-  font-weight: 100;
-  white-space: nowrap;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+    font-weight: 100;
+    white-space: nowrap;
 `;
 
 const orderCSS = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 1rem;
-  font-weight: 100;
-  padding-bottom: 0.4rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+    font-weight: 100;
+    padding-bottom: 0.4rem;
 `;
 
 const taxCSS = css`
-  font-size: 0.8rem;
-  font-weight: 700;
-  color: #777;
-  margin-bottom: 0;
+    font-size: 0.8rem;
+    font-weight: 700;
+    color: #777;
+    margin-bottom: 0;
 `;
 
 const discountCSS = css`
-  font-size: 0.8rem;
-  text-align: center;
-  font-weight: 700;
-  padding: 0 2rem;
-  color: rgb(214, 0, 28);
+    font-size: 0.8rem;
+    text-align: center;
+    font-weight: 700;
+    padding: 0 2rem;
+    color: rgb(214, 0, 28);
 `;
 
 const totalCSS = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-top: 2px solid #222222;
-  margin-top: 1.4rem;
-  padding-top: 1.4rem;
-  padding-bottom: 1.4rem;
-  font-weight: 700;
-  font-size: 1.2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-top: 2px solid #222222;
+    margin-top: 1.4rem;
+    padding-top: 1.4rem;
+    padding-bottom: 1.4rem;
+    font-weight: 700;
+    font-size: 1.2rem;
 `;
 
 const shippingDescriptionCSS = css`
-  background-color: #fff;
-  padding: 1rem;
-  margin-top: 2rem;
+    background-color: #fff;
+    padding: 1rem;
+    margin-top: 2rem;
 `;
 
 export default Order;
