d5c3c7771b3edf6c9c63a6dc787ee90321afbbcf
Constantine <barr.develop@gmail.com>
2020-12-01 03:30:12 +0400

    rework favorites using mobx

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,47 +1,42 @@
-import React from "react";
-import Head from "next/head";
-import Layout from "components/Layout";
-import Preloader from "components/Preloader";
-import List from "features/favorites/List";
-import useSWR from "swr";
-import { useFavorites } from "features/favorites";
+import React from 'react';
+import Head from 'next/head';
+import Layout from 'components/Layout';
+import Preloader from 'components/Preloader';
+import List from 'features/favorites/List';
+import useSWR from 'swr';
+import { stringify } from 'qs';
+import { FavoritesStore } from 'features/favorites';
+import { observer } from 'mobx-react';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-function Favorites() {
-  const { getIds } = useFavorites();
-  const favIds = getIds();
-  const params = new URLSearchParams(
-    favIds.reduce((params, curr) => {
-      params.push(["id", curr]);
-      return params;
-    }, [])
-  );
+const Favorites = observer(() => {
+    const favIds = FavoritesStore.get();
 
-  const { data, error } = useSWR(
-    `/api/products?${params.toString()}`,
-    fetcher
-  );
-  const isLoading = !data && !error;
+    const { data: { data } = {}, error } = useSWR(
+        `/api/products?${stringify({ id: favIds }, { indices: false })}`,
+        fetcher
+    );
+    const isLoading = !data && !error;
 
-  const renderProducts = () => {
-    if (error) return null;
+    const renderProducts = () => {
+        if (error) return null;
 
-    if (!data.length) return "No Products";
+        if (!data.length) return 'No Products';
 
-    return <List products={data} />;
-  };
+        return <List products={data} />;
+    };
 
-  return (
-    <Layout>
-      <Head>
-        <title>Favorites</title>
-      </Head>
+    return (
+        <Layout>
+            <Head>
+                <title>Favorites</title>
+            </Head>
 
-      <h1>Favorites</h1>
-      <div>{isLoading ? <Preloader /> : renderProducts()}</div>
-    </Layout>
-  );
-}
+            <h1>Favorites</h1>
+            <div>{isLoading ? <Preloader /> : renderProducts()}</div>
+        </Layout>
+    );
+});
 
 export default Favorites;
