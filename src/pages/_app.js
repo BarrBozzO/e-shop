b3457f54fe6e2e8becacc7efa401fb27646c1d8d
Constantine <barr.develop@gmail.com>
2020-12-18 03:26:32 +0400

    add fancy page loading effect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,26 @@
-import { UserProvider } from "features/user";
-import "../styles/globals.css";
+import NProgress from 'nprogress';
+// import 'nprogress/nprogress.css';
+import Router from 'next/router';
+import { UserProvider } from 'features/user';
+import '../styles/globals.css';
+
+NProgress.configure({
+    minimum: 0.15,
+    showSpinner: false,
+    template:
+        '<div class="progress-bar" role="bar"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
-  );
+    return (
+        <UserProvider>
+            <Component {...pageProps} />
+        </UserProvider>
+    );
 }
 
 export default MyApp;
